fd3bc684420a0ce0c3e6ae965e98eaca2f5779b9
Artjom plotnikov <artjomplotnikov2017@gmail.com>
2022-09-27 11:09:31 +0300

    Task4

---
+++ src/SPTV21_Task4_ArtjomPlotnikov.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.Random;
+public class SPTV21_Task4_ArtjomPlotnikov {
+        public static void main(String[] args) {
+            int[] numbers;
+            int midlenumber = 0, summa = 0;
+            numbers = new int[100];
+            Random random = new Random();
+            int min = 1, max = 100;
+            int i;
+            for (i = 0; i < numbers.length; i++)
+            {
+                numbers[i] = random.nextInt(max - min + 1) - min;
+                summa +=numbers[i];
+            }
+            midlenumber = summa/100;
+            Arrays.sort(numbers);
+            for (int q = 0; q < 10; q++) {
+                for (int w = 0; w < 10; w++) {
+                    System.out.print(numbers[q * 10 + w] + " ");
+                }
+                System.out.print("\n");
+            }
+            System.out.println("Среднее арифметическое: " + midlenumber);
+        }
+    }
